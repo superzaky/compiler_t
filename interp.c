b9c6ee5e4364686d727d16c563c1e8c275f4fb10
superzaky <superkytoz@gmail.com>
2025-01-26 22:56:06 +0100

    Comment

--- interp.c
+++ interp.c
@@ -11,33 +11,33 @@ static char *ASTop[] = { "+", "-", "*", "/" };
 // operators in it and return
 // a final value.
 int interpretAST(struct ASTnode *n) {
-    int leftval, rightval;
+  int leftval, rightval;
 
-    // Get the left and right sub-tree values
-    if (n->left)
-        leftval = interpretAST(n->left);
-    if (n->right)
-        rightval = interpretAST(n->right);
+  // Get the left and right sub-tree values
+  if (n->left)
+    leftval = interpretAST(n->left);
+  if (n->right)
+    rightval = interpretAST(n->right);
 
-    // Debug: Print what we are about to do
-    if (n->op == A_INTLIT)
-        printf("int %d\n", n->intvalue);
-    else
-        printf("%d %s %d\n", leftval, ASTop[n->op], rightval);
+  // Debug: Print what we are about to do
+  // if (n->op == A_INTLIT)
+  //   printf("int %d\n", n->intvalue);
+  // else
+  //   printf("%d %s %d\n", leftval, ASTop[n->op], rightval);
 
-    switch (n->op) {
+  switch (n->op) {
     case A_ADD:
-        return (leftval + rightval);
+      return (leftval + rightval);
     case A_SUBTRACT:
-        return (leftval - rightval);
+      return (leftval - rightval);
     case A_MULTIPLY:
-        return (leftval * rightval);
+      return (leftval * rightval);
     case A_DIVIDE:
-        return (leftval / rightval);
+      return (leftval / rightval);
     case A_INTLIT:
-        return (n->intvalue);
+      return (n->intvalue);
     default:
-        fprintf(stderr, "Unknown AST operator %d\n", n->op);
-        exit(1);
-    }
+      fprintf(stderr, "Unknown AST operator %d\n", n->op);
+      exit(1);
+  }
 }
